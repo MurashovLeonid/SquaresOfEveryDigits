87f128468be123d24cc52f60e7a84e436443b9ea
Леонид Мурашов <lenja-d@yandex.ru>
2021-10-03 20:14:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+
+namespace DigitsSquarer
+{
+    class ArrayAnalyser
+    {
+        public static int SquareDigits(int n) =>
+            Convert.ToInt32(string.Join("", n.ToString().Select(x => x - '0').ToArray().Select(y => y * y).ToArray()));
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9119;
+
+            Console.WriteLine(ArrayAnalyser.SquareDigits(n));
+        }
+    }
+}
